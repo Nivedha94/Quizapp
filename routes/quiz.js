90998e04eaafb3f4f7ef5579234fe77851bff7b7
“Victoria <victoriahall.dev@gmail.com>
2022-01-11 05:00:37 +0000

    added routes, made some logic for quiz functionality, added some css

---
+++ routes/quiz.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/create', (req, res) => {
+  res.render('createQuiz')
+});
+
+router.get('/start', (req, res) => {
+  res.render('startQuiz')
+});
+
+router.get('/play', (req, res) => {
+  res.render('quiz-game')
+});
+
+module.exports = router;
